9303067e52bea2c5b1721debe1d8235ee412bcde
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-25 23:11:27 -0300

    Set current question when data is loaded

--- src/providers/data/index.js
+++ src/providers/data/index.js
@@ -1,12 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const dataContext = createContext();
 
 const ProvideData = ({ children }) => {
   const [data, setData] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState({});
+
+  useEffect(() => {
+    setCurrentQuestion(data[0]);
+  }, [data]);
 
   return (
-    <dataContext.Provider value={{ data, setData }}>
+    <dataContext.Provider value={{ data, setData, currentQuestion }}>
       {children}
     </dataContext.Provider>
   );
