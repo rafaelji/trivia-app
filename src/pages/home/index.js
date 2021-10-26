31ab94e3352f0229f1d2675fe7bb96009593f896
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-25 22:47:40 -0300

    Code refactor on styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   const { loading } = useHome();
 
   return (
-    <div className="home-container">
+    <div className="container">
       <h1>Welcome to the Trivia Challenge!</h1>
       <p>You will be presented with 10 True or False questions.</p>
       <p>Can you score 100%?</p>
