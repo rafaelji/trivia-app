8c218b85f6698b9c814e3ed404e0a170ef608790
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-25 21:31:38 -0300

    Add information to home page and set initial style

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+
 const Home = () => {
-  return "Welcome home";
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Trivia Challenge!</h1>
+      <p>You will be presented with 10 True or False questions.</p>
+      <p>Can you score 100%?</p>
+      <Link>Begin</Link>
+    </div>
+  );
 };
 
 export default Home;
