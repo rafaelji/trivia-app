93735ff46941cb165cf4a8d21044a55235d7729f
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-26 11:10:11 -0300

    Create protected route

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/home";
 import { QUIZ, HOME, RESULT } from "./config/routes";
 import Quiz from "./pages/quiz";
 import Result from "./pages/result";
 import ProvideData from "./providers/data";
+import useData from "./hooks/data";
 
 const Router = () => {
   return (
@@ -13,15 +14,36 @@ const Router = () => {
           <Route exact path={HOME}>
             <Home />
           </Route>
-          <Route path={QUIZ}>
+          <ProtectedRoute path={QUIZ}>
             <Quiz />
-          </Route>
-          <Route path={RESULT}>
+          </ProtectedRoute>
+          <ProtectedRoute path={RESULT}>
             <Result />
-          </Route>
+          </ProtectedRoute>
         </Switch>
       </ProvideData>
     </BrowserRouter>
+  );
+};
+
+const ProtectedRoute = ({ children, ...rest }) => {
+  let { data } = useData();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        data.length > 0 ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: HOME,
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
   );
 };
 
