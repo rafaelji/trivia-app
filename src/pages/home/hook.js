270ef96de30d3456af5fcd9b82b30ba46c701398
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-25 21:58:12 -0300

    Create function to get trivia data

--- src/pages/home/hook.js
+++ src/pages/home/hook.js
@@ -1,9 +1,22 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { getData } from "../../services/Trivia";
 
 const useHome = () => {
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {}, []);
+  const getQuizData = useCallback(async () => {
+    try {
+      setLoading(true);
+      const result = await getData();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getQuizData();
+  }, [getQuizData]);
 
   return { loading };
 };
