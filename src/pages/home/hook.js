16d154a2b98237808bd98edfc746cea612b64be2
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-26 11:43:44 -0300

    Add error handling for better user experience

--- src/pages/home/hook.js
+++ src/pages/home/hook.js
@@ -1,19 +1,42 @@
 import { useCallback, useEffect, useState } from "react";
 import { getData } from "../../services/Trivia";
 import useData from "../../hooks/data";
+import { QUIZ } from "../../config/routes";
+import { Link } from "react-router-dom";
 
 const useHome = () => {
   const { setData, setAnswerResults, setCurrentQuestion } = useData();
   const [loading, setLoading] = useState(false);
+  const [errorHappened, setErrorHappened] = useState(false);
+
+  const showRightAction = () => {
+    if (errorHappened) {
+      return (
+        <>
+          <p>An error happened when trying to load game data.</p>
+          <button onClick={getQuizData}>Try Again</button>
+        </>
+      );
+    }
+
+    if (loading) {
+      return <p>Loading game...</p>;
+    }
+
+    return <Link to={QUIZ}>Begin</Link>;
+  };
 
   const getQuizData = useCallback(async () => {
     try {
+      setErrorHappened(false);
       setLoading(true);
       const result = await getData();
       setData(result);
       setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
+      setErrorHappened(true);
     }
   }, [setData]);
 
@@ -27,7 +50,7 @@ const useHome = () => {
     getQuizData();
   }, [getQuizData]);
 
-  return { loading };
+  return { loading, showRightAction };
 };
 
 export default useHome;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
 import useHome from "./hook";
-import { QUIZ } from "../../config/routes";
 
 const Home = () => {
-  const { loading } = useHome();
+  const { showRightAction } = useHome();
 
   return (
     <div className="container">
       <h1>Welcome to the Trivia Challenge!</h1>
       <p>You will be presented with 10 True or False questions.</p>
       <p>Can you score 100%?</p>
-      {loading ? <p>Loading game...</p> : <Link to={QUIZ}>Begin</Link>}
+      {showRightAction()}
     </div>
   );
 };
