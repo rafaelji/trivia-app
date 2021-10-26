b57c07a765a2b30047a7e4efe0bbb52ed19e7c9a
Rafael Barbosa Silva <r.ji@outlook.com>
2021-10-25 21:56:43 -0300

    Create service to get trivia data

---
+++ src/services/Trivia.js
@@ -0,0 +1,13 @@
+import api from "../config/api";
+
+const getData = async () => {
+  const data = await api.get("?amount=10&difficulty=hard&type=boolean");
+
+  if (data.status === 200) {
+    return data.data.results;
+  }
+
+  throw new Error("Error happened when retrieving data");
+};
+
+export { getData };
